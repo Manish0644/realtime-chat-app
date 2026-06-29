c07dd3b6378cf54295eb98c3b5bd590a63e2a69b
DIWAKAR KUMAR <72044814+Manish0644@users.noreply.github.com>
2026-06-29 19:28:21 +0530

    Initial commit

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-theme") || "coffee",
+
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    set({ theme });
+  },
+}));
+
+
+
+
+
+
+
+
+
+
