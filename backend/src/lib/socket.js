6215268df04691acf7c29a69492a6661f7f3b16e
DIWAKAR KUMAR <72044814+Manish0644@users.noreply.github.com>
2026-07-01 00:45:39 +0530

    Fix socket CORS for production

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,11 +6,23 @@ import express from "express";
 const app=express();
 const server = http.createServer(app);
 
-const io =new Server(server,{
-    cors:{
-        origin:["http://localhost:5173"]
-    }
+// const io =new Server(server,{
+//     cors:{
+//         origin:["http://localhost:5173"]
+//     }
+// });
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-frontend-five-nu.vercel.app",
+    ],
+    credentials: true,
+  },
 });
+
+
 
 export function getReceiverSocketId(userId){
     return userSocketMap[userId];
